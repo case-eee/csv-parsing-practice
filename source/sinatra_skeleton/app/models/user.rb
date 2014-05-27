d422e2649df8372d1d18868b61bf822b78faca80
Casey Ann Cumbow <cumbow8@gmail.com>
2014-05-26 21:02:02 -0500

    major probs

--- source/sinatra_skeleton/app/models/user.rb
+++ source/sinatra_skeleton/app/models/user.rb
@@ -1,9 +1,11 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
+  has_many :usergroups
+  has_many :groups, through: :usergroups
 
 	include BCrypt
-	
-	has_secure_password
-  # Remember to create a migration!
+
+	# has_secure_password = 'a'
+
 end
--- source/sinatra_skeleton/db/seeds.rb
+++ source/sinatra_skeleton/db/seeds.rb
@@ -1,1 +1,24 @@
 require 'faker'
+
+User.create(first_name: "Steve", last_name: "Yim", user_name: "stevieboo", password_hash: "password")
+
+User.create(first_name: "Daniel", last_name: "D", user_name: "dmoney", password_hash: "password")
+
+User.create(first_name: "Sasha", last_name: "G", user_name: "sashy", password_hash: "password")
+
+User.create(first_name: "Parth", last_name: "N", user_name: "pdiddy", password_hash: "password")
+
+
+Usergroup.create(user_id: 1, group_id: 3)
+Usergroup.create(user_id: 2, group_id: 2)
+Usergroup.create(user_id: 3, group_id: 1)
+Usergroup.create(user_id: 4, group_id: 1)
+Usergroup.create(user_id: 1, group_id: 1)
+Usergroup.create(user_id: 2, group_id: 4)
+Usergroup.create(user_id: 4, group_id: 4)
+Usergroup.create(user_id: 1, group_id: 4)
+
+Group.create(name: "Lawyers")
+Group.create(name: "Readers")
+Group.create(name: "Travelers")
+Group.create(name: "Doctors")
